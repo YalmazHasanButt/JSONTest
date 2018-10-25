fa8ae757b16a4f53938cbb748536787c15f77db1
yalmazhasanbutt <yalmazhasanbutt@gmail.com>
2018-10-25 21:22:08 +0500

    Initial commit

---
+++ app/src/main/java/funkymaster/com/jsontest/FetchData.java
@@ -0,0 +1,37 @@
+package funkymaster.com.jsontest;
+
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class FetchData extends AsyncTask<Void, Void, Void> {
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+
+        try {
+            URL url = new URL("https://api.myjson.com/bins/zmcas");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            InputStream inputStream = connection.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+    }
+}
---
+++ app/src/main/java/funkymaster/com/jsontest/MainActivity.java
@@ -0,0 +1,17 @@
+package funkymaster.com.jsontest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //JSON test
+    //Firebase test
+    //GIT Hub Test
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
